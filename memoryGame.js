dae3fd6bf4c6513e03f76ffbb9bd0ccef344500e
Eitan <8an.akrish@gmail.com>
2021-11-16 19:18:21 +0200

    mmory

---
+++ memoryGame.js
@@ -0,0 +1,52 @@
+const cards = [
+    { name: `A` },
+    { name: `B` },
+    { name: `C` },
+    { name: `A` },
+    { name: `B` },
+    { name: `C` },
+];
+
+function randomNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function swap(arr, i, j) {
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
+
+
+function shuffle(arr) {
+    for (let i = 0; i < 100; i++) {
+        let a1 = randomNum(0, arr.length)
+        let a2 = randomNum(0, arr.length)
+        if (a1 == a2) {
+            i--;
+            continue
+        }
+        swap(arr, a1, a2)
+    }
+}
+
+function main() {
+    console.log(...cards);
+    shuffle(cards)
+    console.log(cards);
+}
+main()
+
+// const check = (iCards1, iCards2) {
+//     if (iCards1 == iCards2) {
+//         return alert(`same card chosen twice`)
+//     } else if (cards[iCards1][`name`] == cards[iCards2][`name`]) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+const check = (iCards1, iCards2) => (iCards1 == iCards2) ? alert(`same card chosen twice`) : (cards[iCards1][`name`] == cards[iCards2][`name`]);
